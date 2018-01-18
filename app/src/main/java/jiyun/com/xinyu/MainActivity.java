805e71d4fae6598739fc9ec919bccffe41427b8f
97387 <97387406@qq.com>
2018-01-18 10:35:26 +0800

    Initial commit

---
+++ app/src/main/java/jiyun/com/xinyu/MainActivity.java
@@ -0,0 +1,14 @@
+package jiyun.com.xinyu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        String s="王心雨";
+    }
+}
